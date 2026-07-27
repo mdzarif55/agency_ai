a67d31d732e34b59fc72b0d0965b878510e8f55b
mdzarif55 <muhtasimzarif55@gmail.com>
2026-07-28 02:03:03 +0600

    navbar complete

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,5 +1,6 @@
 import React, { useState } from 'react'
 import assets from '../assets/assets'
+import ThemeToggleButton from './ThemeToggleButton'
 
 const Navbar = ({ theme, setTheme }) => {
 
@@ -20,6 +21,8 @@ const Navbar = ({ theme, setTheme }) => {
             </div>
 
             <div className='flex items-center gap-2 sm:gap-4'>
+
+                <ThemeToggleButton theme={theme} setTheme={setTheme}/>
 
                 <img src={theme === 'dark' ? assets.menu_icon_dark : assets.menu_icon}  alt="" onClick={()=> setSidebarOpen(true)} className='w-8 sm:hidden' />
 
