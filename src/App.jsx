6bc323b5d11f708380110c5f457c636eac5bfb01
mdzarif55 <muhtasimzarif55@gmail.com>
2026-07-28 21:09:39 +0600

    trusted by complete

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import React, { useState } from 'react'
 import Navbar from './components/Navbar'
 import Hero from './components/Hero'
+import TrustedBy from './components/TrustedBy'
 
 const App = () => {
 
@@ -8,6 +9,7 @@ const App = () => {
   return (
     <div className='dark:bg-black relative'> <Navbar theme={theme} setTheme={setTheme} /> 
     <Hero/>
+    <TrustedBy/>
     </div>
     
   )
